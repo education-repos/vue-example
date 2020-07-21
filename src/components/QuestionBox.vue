d88bdf209216c778710bb6c9689b278dfe6f58c3
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-07-21 13:54:16 +0400

    add answers to template, select method and styles

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -9,9 +9,18 @@
 
                     <hr class="my-4">
 
-                    <p>
-                        List of answers
-                    </p>
+                    <b-list-group>
+                        <b-list-group-item
+                            v-for="(answer, index) in answers"
+                            :key="index"
+                            @click="selectAnswer(index)"
+                            :class="[selectedIndex === index ? 'selected' : '']"
+                        >
+                            {{ answer }}
+                        </b-list-group-item>
+                    </b-list-group>
+
+                    <hr class="my-4">
 
                     <b-button variant="primary">Submit</b-button>
                     <b-button @click="next" variant="success">Next</b-button>
@@ -27,10 +36,51 @@
         props: {
             currentQuestion: Object,
             next: Function
+        },
+        data() {
+            return {
+                selectedIndex: null
+            }
+        },
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index
+            }
+        },
+        computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers]
+                answers.push(this.currentQuestion.correct_answer)
+                return answers
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .jumbotron {
+        padding: 2rem;
+    }
+    .btn {
+        margin-right: 1rem;
+    }
+    .list-group-item {
+        transition: .1s all;
+    }
+    .list-group-item:hover {
+        cursor: pointer;
+        background: lightslategray;
+        color: white;
+    }
+    .selected {
+        background: lightblue;
+    }
+    .correct {
+        background: lightgreen;
+        color: white;
+    }
+    .incorrect {
+        background: lightcoral;
+        color: white;
+    }
 </style>
